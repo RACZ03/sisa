065d87513964fd8deb4fae7600fb0bf3e0c055fd
Roberto Calero <roberto.calero@claro.com.ni>
2023-08-14 11:02:32 -0600

    Update dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -92,11 +92,7 @@
     <div class="col-lg-8 mx-auto">
       <div class="card z-index-2">
         <div class="card-header pb-0">
-          <h6>Sales overview</h6>
-          <p class="text-sm">
-            <i class="fa fa-arrow-up text-success"></i>
-            <span class="font-weight-bold">4% more</span> in 2021
-          </p>
+          <h6>Inventarios</h6>
         </div>
         <div class="card-body p-3">
           <div class="chart">
